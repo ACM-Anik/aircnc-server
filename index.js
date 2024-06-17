aff8b8d2fba7ad3ac96776d013de42391f7a9e2f
ACM-Anik <anikmojumder5252@gmail.com>
2024-06-17 23:30:32 +0600

    Connected to MongoBD and added .env to hide info

--- index.js
+++ index.js
@@ -14,6 +14,35 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vxma4ez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+
+async function run() {
+  try {
+    const usersCollection = client.db('aircnc').collection('users');
+    const roomsCollection = client.db('aircnc').collection('rooms');
+    const bookingsCollection = client.db('aircnc').collection('bookings');
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
   res.send('AirCNC Server is running..')
