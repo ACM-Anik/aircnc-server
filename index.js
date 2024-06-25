46c44ab591e0021c5e04d458bb0540d086387505
ACM-Anik <anikmojumder5252@gmail.com>
2024-06-25 23:10:17 +0600

    A post api implemented to save rooms data

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const port = process.env.PORT || 5000;
 
 // middleware
 const corsOptions = {
-  origin: '*',
-  credentials: true,
-  optionSuccessStatus: 200,
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -18,51 +18,62 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vxma4ez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
 })
 
 async function run() {
-  try {
-    const usersCollection = client.db('aircnc').collection('users');
-    const roomsCollection = client.db('aircnc').collection('rooms');
-    const bookingsCollection = client.db('aircnc').collection('bookings');
+    try {
+        const usersCollection = client.db('aircnc').collection('users');
+        const roomsCollection = client.db('aircnc').collection('rooms');
+        const bookingsCollection = client.db('aircnc').collection('bookings');
 
-    // Save user email and role in BD:-
-    app.put('/users/:email', async (req, res) => {
-        const email = req.params.email;
-        const user = req.body;
-        const query = {email: email};
-        const options = {upsert: true};
-        const updateDoc = {
-            $set: user,
+        // Save user email and role in DB:-
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const query = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
 
-        };
-        const result = await usersCollection.updateOne(query, updateDoc, options);
-        console.log(result);
-        res.send(result);
-    });
+            };
+            const result = await usersCollection.updateOne(query, updateDoc, options);
+            console.log(result);
+            res.send(result);
+        });
 
-    // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
-    console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    )
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Save a room in DB:-
+        app.post('/rooms', async (req, res) => {
+            const room = req.body;
+            console.log(room);
+            const result = await roomsCollection.insertOne(room);
+            res.send(result);
+        });
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db('admin').command({ ping: 1 })
+        console.log(
+            'Pinged your deployment. You successfully connected to MongoDB!'
+        )
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
-  res.send('AirCNC Server is running..')
+    res.send('AirCNC Server is running..')
 });
 
 app.listen(port, () => {
-  console.log(`AirCNC is running on port ${port}`)
+    console.log(`AirCNC is running on port ${port}`)
 });
