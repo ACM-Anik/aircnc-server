615b7089b15528e708409efde1672f100f144753
ACM-Anik <anikmojumder5252@gmail.com>
2024-08-14 23:49:14 +0600

    A DELETE API implemented to delete a room

--- index.js
+++ index.js
@@ -98,6 +98,14 @@ async function run() {
             res.send(result);
         });
 
+        // Delete a room:--
+        app.delete('/rooms/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await roomsCollection.deleteOne(query);
+            res.send(result);
+        });
+
 
         //-----------BookingsCollection:-----------
         // Get bookings for quest
